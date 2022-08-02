9fbff43b6a71cf5ea4d5850446b7fadf4f2493bc
smitayadav2709 <41377619+smitayadav2709@users.noreply.github.com>
2022-08-03 00:47:07 +0530

    Add files via upload

---
+++ main.tf
@@ -0,0 +1,14 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "aws"
+      
+    }
+    
+  }
+}
+provider "aws" {
+  region="ap-south-1"
+  access_key = "AKIAQJC6PSU6PFO3BBWJ"
+  secret_key = "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"
+}
---
+++ variables.tf
@@ -0,0 +1,9 @@
+ variable "subnet_id_1" {
+  type = string
+  default = "subnet-0a3d944cabde7101a"
+ }
+ 
+ variable "subnet_id_2" {
+  type = string
+  default = "subnet-0b0d8d96008a90853"
+ }
